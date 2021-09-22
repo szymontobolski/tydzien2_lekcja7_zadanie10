dba8f8d6d7aa22e20f22f9a6ab72bf0ae83fc84e
Szymon <Szymon@LAPTOP-2DIS65OF>
2021-09-22 13:10:40 +0200

    Add project files.

---
+++ tydzien2_lekcja7_zadanie10/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace tydzien2_lekcja7_zadanie10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            int a = 40;
+            int b = 55;
+            int c = 65;
+
+            if (c < a + b) 
+            Console.WriteLine("Można zbudować trójkąt");
+            else
+                Console.WriteLine("Nie można zbudować trójkąta");
+
+            Console.ReadKey();
+
+        }
+    }
+}
